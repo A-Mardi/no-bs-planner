85fdd042d4bd07ac2f8da3c903ba7c296e8de67f
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2024-09-13 14:34:22 -0400

    Add project files.

---
+++ NoBsPlanner/Models/PlannerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoBSPlanner.Models;
+
+namespace NoBSPlanner.Models
+{
+    public class PlannerContext : DbContext
+    {
+        public PlannerContext(DbContextOptions<PlannerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PlannerItem> PlannerItems { get; set; }
+    }
+}
+
