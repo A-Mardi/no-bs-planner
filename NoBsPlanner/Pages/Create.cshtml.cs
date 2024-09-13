bd2284e5c8790409e27181a73dc6998ccbd3a2ce
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2024-09-13 14:54:36 -0400

    Add Razor pages(CRUD) for managing the planner items, and test functionality

---
+++ NoBsPlanner/Pages/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NoBSPlanner.Models;
+
+namespace NoBSPlanner.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly NoBSPlanner.Models.PlannerContext _context;
+
+        public CreateModel(NoBSPlanner.Models.PlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public PlannerItem PlannerItem { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.PlannerItems.Add(PlannerItem);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- NoBsPlanner/Pages/Index.cshtml.cs
+++ NoBsPlanner/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NoBSPlanner.Models;
 
 namespace NoBSPlanner.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly NoBSPlanner.Models.PlannerContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(NoBSPlanner.Models.PlannerContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<PlannerItem> PlannerItem { get;set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            PlannerItem = await _context.PlannerItems.ToListAsync();
         }
     }
 }
